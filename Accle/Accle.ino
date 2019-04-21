48e37c0d1e2684b97e65396dc07b8fc8cd109ef1
nguyenvandai61 <crystallifestyle05@gmail.com>
2019-04-21 11:24:40 +0700

    Chu thich 1

--- Accle/Accle.ino
+++ Accle/Accle.ino
@@ -31,18 +31,21 @@ void setup() {
   Wire.write(0x3B); 
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr, 14, true); 
+
+  // Doc cac gia tri gia toc
   AcX = Wire.read() << 8 | Wire.read(); 
   AcY = Wire.read() << 8 | Wire.read(); 
   AcZ = Wire.read() << 8 | Wire.read();
-  GyX = Wire.read() << 8 | Wire.read();
-  GyY = Wire.read() << 8 | Wire.read();
-  GyZ = Wire.read() << 8 | Wire.read();
+//  GyX = Wire.read() << 8 | Wire.read();
+//  GyY = Wire.read() << 8 | Wire.read();
+//  GyZ = Wire.read() << 8 | Wire.read();
 
 
   int xAng = map(AcX, minVal, maxVal, -90, 90);
   int yAng = map(AcY, minVal, maxVal, -90, 90);
   int zAng = map(AcZ, minVal, maxVal, -90, 90);
 
+  // Chuyen sang do deg
   x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
   y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
   z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
