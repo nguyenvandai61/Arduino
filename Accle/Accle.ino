31d53a1dcd03ea00ded4f992427b0e8338c3957f
nguyenvandai61 <crystallifestyle05@gmail.com>
2019-04-22 19:11:30 +0700

    OK gia toc

--- Accle/Accle.ino
+++ Accle/Accle.ino
@@ -8,11 +8,11 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  lcd.begin();
+  lcd.init();
   lcd.backlight();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
-  s.attach(9);
+  s.attach(2);
 }
 
 void loop() {
@@ -21,9 +21,12 @@ void loop() {
   Serial.println(mpu6050.getAngleX());
   Serial.print("\tangleY : ");
   Serial.println(mpu6050.getAngleY());
+  
+  lcd.setCursor(0, 0);
+  lcd.print(mpu6050.getAngleY());
   lcd.print(round(mpu6050.getAngleY() +90));
   s.write(mpu6050.getAngleY()+90);
 
-  delay(10);
-  
+  delay(500);
+  lcd.clear();
 }
